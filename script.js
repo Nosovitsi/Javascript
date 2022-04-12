66edd2c21b9ff836b4ad940d5d7788456f0312fe
Nosovitski <nosovitski.gleb@gmail.com>
2022-04-13 00:17:35 +0300

    good work

--- script.js
+++ script.js
@@ -6,21 +6,60 @@
 
 //let a = prompt("Назови свое имя", "Например, Вася");
 //let age = prompt("Сколько тебе лет?", 50);
-//alert(`${a} тебе ${age} лет `);
+//alert("a + " тебе" + age " лет ");
 
 //"use strict";
 
-let num1 = prompt("Введите первое число", 0);
-let num2 = prompt("Введите второе число", 0);
-let action = prompt("Выберите действие", "Например + - * /");
-if (action == "+") {
-    alert(Number(num1) + Number(num2));
-} else if (action == "-") {
-    alert(Number(num1) - Number(num2));
-} else if (action == "*") {
-    alert(Number(num1) * Number(num2));
-} else if (action == "/") {
-    alert(Number(num1) / Number(num2));
-} else {
-    alert("Вы не выбрали действие");
-}
+// let num1 = prompt("Введите первое число", 0);
+// let num2 = prompt("Введите второе число", 0);
+// let action = prompt("Выберите действие", "Например + - * /");
+// if (action == "+") {
+//   alert(Number(num1) + Number(num2));
+// } else if (action == "-") {
+//    alert(Number(num1) - Number(num2));
+// } else if (action == "*") {
+//   alert(Number(num1) * Number(num2));
+// } else if (action == "/") {
+//    alert(Number(num1) / Number(num2));
+// } else {
+//    alert("Вы не выбрали действие");
+// }
+
+// let a = prompt("Назови свое имя", "Например, Вася");
+// let age = prompt("Сколько тебе лет?", 50);
+// alert(`${a} тебе ${age} лет `);
+
+// let a = prompt("введите первое число", 0);
+// let b = prompt("введите второе число", 0);
+// alert(`${a} - ${b}`);
+
+// let a = Number(prompt("введите первое число", 0));
+// let b = Number(prompt("введите второе число", 0));
+// alert(`${a + b}`);
+
+// let a = prompt("введите первое число", 0);
+// let b = prompt("введите второе число", 0);
+// alert(`${Number(a + b)}`);
+
+// let a = prompt("введите первое число", 0);
+// let b = Number(prompt("введите второе число", 0));
+// let y = Number(a + b);
+// alert(y + b + a);
+
+// let a = prompt("введите первое число", 0);
+// let b = prompt("введите второе число", 0);
+// let y = prompt(`${a + b}`);
+// alert(y + a);
+
+// let a = prompt("введите первое число", 0);
+// let b = prompt("введите второе число", 0);
+// let y = prompt(`${"Придумай новое число"}`);
+// alert(y + a);
+
+// let a = Number(prompt("введите первое число", 0));
+// let b = Number(prompt("введите второе число", 0));
+// alert(`${a-b}`);
+
+let a = prompt("введите первое число", 0);
+let b = prompt("введите второе число", 0);
+alert(`${Number(a)-Number(b)}`);
