99ef0ecc25189fe0849e6a2ad3366cc4e224abe8
Nosovitski <nosovitski.gleb@gmail.com>
2022-04-15 12:49:06 +0300

    work in progress

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-//let age = prompt("Сколько тебе лет?");
-//alert("тебе " + age + " лет");
+// let age = prompt("Сколько тебе лет?");
+// alert("тебе " + age + " лет");
 
-//let a = confirm("How are you?");
-//alert(a);
+// let a = confirm("How are you?");
+// alert(a);
 
-//let a = prompt("Назови свое имя", "Например, Вася");
-//let age = prompt("Сколько тебе лет?", 50);
-//alert("a + " тебе" + age " лет ");
+// let a = prompt("Назови свое имя", "Например, Вася");
+// let age = prompt("Сколько тебе лет?", 50);
+// alert(a + " тебе " + age + " лет ");
 
 //"use strict";
 
-let num1 = prompt("Введите первое число", 0);
-let num2 = prompt("Введите второе число", 0);
-let action = prompt("Выберите действие", "Например + - * /");
-if (action == "+") {
-    alert(Number(num1) + Number(num2));
-} else if (action == "-") {
-    alert(Number(num1) - Number(num2));
-} else if (action == "*") {
-    alert(Number(num1) * Number(num2));
-} else if (action == "/") {
-    alert(Number(num1) / Number(num2));
-} else {
-    alert("Вы не выбрали действие");
-}
+// let num1 = prompt("Введите первое число", 0);
+// let num2 = prompt("Введите второе число", 0);
+// let action = prompt("Выберите действие", "Например + - * /");
+// if (action == "+") {
+//     alert(Number(num1) + Number(num2));
+// } else if (action == "-") {
+//     alert(Number(num1) - Number(num2));
+// } else if (action == "*") {
+//     alert(Number(num1) * Number(num2));
+// } else if (action == "/") {
+//     alert(Number(num1) / Number(num2));
+// } else {
+//     alert("Вы не выбрали действие");
+// }
 
 // let a = prompt("Назови свое имя", "Например, Вася");
 // let age = prompt("Сколько тебе лет?", 50);
@@ -63,3 +63,10 @@ if (action == "+") {
 // let a = prompt("введите первое число", 0);
 // let b = prompt("введите второе число", 0);
 // alert(`${Number(a)-Number(b)}`);
+
+
+let login = prompt("Введите свой логин");
+let password = prompt("Введите свой пароль");
+if ((login === "gleb") && (password === "123")) { // || - означают ИЛИ;   && - означает И;
+    alert("Вход выполнен!")
+} else { alert("Проверьте введенные данные") }
