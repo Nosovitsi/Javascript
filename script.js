338f65f75fff10ecd499b3c987629a58a6f28189
Nosovitski <nosovitski.gleb@gmail.com>
2022-04-19 18:14:04 +0300

    change

--- script.js
+++ script.js
@@ -10,20 +10,20 @@
 
 //"use strict";
 
-// let num1 = prompt("Введите первое число", 0);
-// let num2 = prompt("Введите второе число", 0);
-// let action = prompt("Выберите действие", "Например + - * /");
-// if (action == "+") {
-//     alert(Number(num1) + Number(num2));
-// } else if (action == "-") {
-//     alert(Number(num1) - Number(num2));
-// } else if (action == "*") {
-//     alert(Number(num1) * Number(num2));
-// } else if (action == "/") {
-//     alert(Number(num1) / Number(num2));
-// } else {
-//     alert("Вы не выбрали действие");
-// }
+let num1 = prompt("Введите первое число", 0);
+let num2 = prompt("Введите второе число", 0);
+let action = prompt("Выберите действие", "Например + - * /");
+if (action == "+") {
+    alert(Number(num1) + Number(num2));
+} else if (action == "-") {
+    alert(Number(num1) - Number(num2));
+} else if (action == "*") {
+    alert(Number(num1) * Number(num2));
+} else if (action == "/") {
+    alert(Number(num1) / Number(num2));
+} else {
+    alert("Вы не выбрали действие");
+}
 
 // let a = prompt("Назови свое имя", "Например, Вася");
 // let age = prompt("Сколько тебе лет?", 50);
@@ -65,8 +65,8 @@
 // alert(`${Number(a)-Number(b)}`);
 
 
-let login = prompt("Введите свой логин");
-let password = prompt("Введите свой пароль");
-if ((login === "gleb") && (password === "123")) { // || - означают ИЛИ;   && - означает И;
-    alert("Вход выполнен!")
-} else { alert("Проверьте введенные данные") }
+// let login = prompt("Введите свой логин");
+// let password = prompt("Введите свой пароль");
+// if ((login === "gleb") && (password === "123")) { // || - означают ИЛИ;   && - означает И;
+//     alert("Вход выполнен!")
+// } else { alert("Проверьте введенные данные") }
