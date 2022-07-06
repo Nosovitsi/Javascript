0c89dec08aa0f3599b84d748cd02855f2b3b303a
Nosovitski <nosovitski.gleb@gmail.com>
2022-07-06 17:06:01 +0300

    Homework

---
+++ Main_JavaScript/script.js
@@ -0,0 +1,5 @@
+let width = Number(prompt("Введите значение ширины"));
+let lenght = Number(prompt("Введите значение длины"));
+let area = width * lenght;
+let size = (2 * (width + lenght));
+console.log("Площадь равна: " + area + " Периметр равен: " + size);
